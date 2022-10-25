90553b382fe2fdb90616483e16aef410e42c0f4b
SaulKaique <Saul.silva@etec.sp.gov.br>
2022-10-25 16:39:40 -0300

    Terminado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("--- Ano Bissexto --- \n");
+int Ano = Convert.ToInt32(Console.ReadLine());
+
+bool Divisivelpor400 = Ano % 400 == 0 ;
+bool Divisivelpor100 = Ano %  100 == 0 ;
+bool Divisivelpor4 = Ano % 4 == 0 ;
+
+
+if (Divisivelpor400 || (Divisivelpor4 && ! Divisivelpor100)){
+
+
+     Console . WriteLine ( $" \n { Ano } é bissexto. " );
+}
+
+else
+{
+
+    Console . WriteLine ( $" \n { Ano } não é bissexto. " );
+     
+}
